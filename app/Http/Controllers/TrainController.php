5d89f74485e54dccd76941a66b41e08ecda0544e
alessandroordetti <alessandro.ordetti@gmail.com>
2022-05-16 17:38:32 +0200

    Adding href to trains

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -9,11 +9,17 @@ use Illuminate\Http\Request;
 class TrainController extends Controller
 {
     public function index(){
-        $trainsModel= new Train();
+        $trainsModel = new Train();
         $trains = $trainsModel::paginate(20);
 
         $getTime = Train::where('departureTime', '=' , '2022-05-13 00:00:00')->get();   
 
         return view('trains.index', ['trains' => $trains, 'getTime' => $getTime]);
+    }
+
+    public function show($id){
+        $train = Train::findOrFail($id);
+
+        return view('trains.show', ['train' => $train]);
     }
 }
--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -4,15 +4,15 @@
     <div class="container">
         <div class="row mb-5">
                 @foreach ($trains as $train)
-                <div class="col-3">
-                    <div class="card" style="width: 18rem;">
-                        <div class="card-body">
-                            <h5 class="card-title">{{$train->trainCode}}</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
-                        </div>
+                    <div class="col-3">
+                            <div class="card" style="width: 18rem;">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{$train->trainCode}}</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    <a href='{{url("/$train->id")}}' class="btn btn-primary">Vai ai dettagli del treno</a>
+                                </div>
+                            </div>
                     </div>
-                </div>
                 @endforeach
         </div>
 
